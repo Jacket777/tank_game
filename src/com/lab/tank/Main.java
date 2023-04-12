88a70c216e79cdc9efb77b6b25afb1c90892021e
Jacket777 <hxqsu37@sina.com>
2023-04-12 21:10:20 +0800

    tt

---
+++ src/com/lab/tank/Main.java
@@ -0,0 +1,21 @@
+package com.lab.tank;
+
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+/**
+ * frist version
+ * @author n'n
+ * 2023-04-12
+ *
+ */
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		TankFrame f = new TankFrame();
+		while(true) {
+			Thread.sleep(50);
+			f.repaint();
+		}
+	}
+}
